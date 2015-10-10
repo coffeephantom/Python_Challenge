a521833d71acac3d8b0272832df5bd5ea0c3dfe7
coffeephantom <coffeephantom@users.noreply.github.com>
2015-10-10 17:21:03 +0800

    shuffle_pic解决方案，继续了解python文件处理、图像处理内容

---
+++ pic_shuffle.py
@@ -0,0 +1,1 @@
+__author__ = 'Administrator'with open('evil2.gfx','rb') as f:    content = f.read()print len(content)print content[:25]print type(content)file_types = ['jpg', 'png', 'gif', 'png', 'jpg']for i in range(5):    with open("%s%d.%s" % ('evil2_', i, file_types[i]), 'wb') as f:        f.write(content[i::5])
