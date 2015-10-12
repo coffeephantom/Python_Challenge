8c496cd42d10735ae129c10ad85c87c480574446
coffeephantom <coffeephantom@users.noreply.github.com>
2015-10-12 16:07:57 +0800

    添加注释

--- sprial_image.py
+++ sprial_image.py
@@ -12,8 +12,11 @@ print "height = %s" % (height)
 print "width = %s" % (width)
 print "mode = %s" % (img.mode)
 
+# 代表图片四角
 dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 x, y, z = -1, 0, 0
+
+# 遍历新图中的所有点
 for i in range(200):
     d = dirs[i % 4]
     print d
@@ -22,6 +25,7 @@ for i in range(200):
         y += d[1]
         print (x,y)
 
+       # 将原图中的像素点绘制到新图中，原图的纵坐标只有0,1
         img_new.putpixel((x,y), img.getpixel((z,0)))
         z += 1
 
